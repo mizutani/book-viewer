6688143ff5e173fd35adbbeb76fa7f04eb876a3b
erefy <erefy.yami.mio@gmail.com>
2016-12-11 15:46:48 +0900

    画像タイトル設定

--- src/components/screen/screen.js
+++ src/components/screen/screen.js
@@ -1,11 +1,12 @@
 import React, { PropTypes } from 'react';
-import {Card, CardMedia} from 'material-ui/Card';
+import {Card, CardMedia, CardTitle} from 'material-ui/Card';
 import Image from 'lib_modules/index';
 import styles from './screen.css';
 
 const Screen = ({ path }) => (
   <div className={styles.screen}>
     <Card>
+      <CardTitle title="画像タイトル" subtitle="画像サブタイトル" />
       <CardMedia>
         <Image src={path} />
       </CardMedia>
